3a5f82f9a2bfdb684eadd50569999b070a4999ca
jescutia <jescutia@mail.csuchico.edu>
2017-04-26 21:12:51 -0700

    added function noViolations()

--- solver.cpp
+++ solver.cpp
@@ -1,5 +1,23 @@
 #include "solver.h"
 
+// initializes the char reference array (m_ref)
+Solver::Solver()
+{
+  int i = 0;
+  // add char values 0 to 9
+  for(int c = 48; c <= 57; ++c)
+  {
+    m_ref[i] = c;
+    ++i;
+  }
+  // add char values A to F
+  for(int c = 65; c <= 70; ++c)
+  {
+    m_ref[i] = c;
+    ++i;
+  }
+}
+
 // function parses input file and inserts values into the grid
 void Solver::parse_input(string filename)
 {
@@ -49,6 +67,7 @@ void Solver::solve_sudoku()
 
 }
 
+// check if row has no violations
 bool Solver::checkRow(char value, int rowNum)
 {
   for(int i = 0; i < 16; i++)
@@ -61,15 +80,28 @@ bool Solver::checkRow(char value, int rowNum)
   return true;
 }
 
+// check if column has no violations
+bool checkCol(char value, int colNum)
+{
+  
+}
+
+// check if block has no violations
 bool Solver::checkBlock(char value, int rowNum, int colNum)
 {
   for(int y = 0; y < n; ++y)
 	{
 		for(int x = 0; x < n; ++x)
 		{
-			if(m_grid[x+rowNum][y+colNum] == value)
+			if(m_grid[x+rowNum][y+colNum].m_value == value)
 				return false;
 		}
 	}
 	return true;
+}
+
+// returns true if there are no violations for row, column, and, block
+bool Solver::noViolations(char val, int rowNum, int colNum)
+{
+  return checkRow(val, rowNum) && checkCol(val, colNum) && checkBlock(val, rowNum, colNum);
 }
